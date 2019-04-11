ab85bc7e0d5570a53ee0e3574f87d209eeaacc0b
Luke Secomb <lukessecomb@gmail.com>
2019-04-11 23:21:43 +1000

    js to es6 / update repo link

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-// add image url button 
-var buttonGroup = document.getElementsByClassName("sc-button-group sc-button-group-medium");
+let buttonGroup, specificGroup, imageButtonDiv, image, listOfUrls, theImageLink, fixedURL, newButton;
 
-var specificGroup = buttonGroup[0];
+buttonGroup = document.querySelectorAll(".sc-button-group, .sc-button-group-medium");
+specificGroup = buttonGroup[0];
 
-var imageButtonDiv = '<button type="button" id="getMyUrlImagePlease" class="sc-button-like sc-button sc-button-medium sc-button-responsive" tabindex="0" aria-haspopup="true" role="button" aria-owns="dropdown-button-3255" title="More">Image Url</button>';
-
+imageButtonDiv = '<button type="button" id="getMyUrlImagePlease" class="sc-button-cta sc-button sc-button-medium sc-button-responsive" tabindex="0" aria-haspopup="true" role="button" aria-owns="dropdown-button-3255" title="More">Image Url</button>';
 specificGroup.insertAdjacentHTML('afterend', imageButtonDiv);
 
 function getMyImagePlease() {
-    console.log("function has been run");
-    var image = document.getElementsByClassName("image m-sound image__lightOutline readOnly customImage interactive sc-artwork")[0].innerHTML;
     // returns "background-image: url("https://i1.sndcdn.com/avatars-000460662156-17m44t-t50x50.jpg"); width: 40px; height: 40px; opacity: 1;"
+    // let image = document.querySelectorAll(".image, .m-sound, .image__lightOutline, .readOnly, .customImage, .interactive, .sc-artwork")[0].innerHTML;
+    image = document.querySelectorAll(".listenArtworkWrapper")[0].querySelectorAll('.image > span')[0].style.backgroundImage;
 
-    var listOfUrls = image.match(/\b(https):\/\/(\S*)\b/);
     // returns list of image urls matching className 
+    listOfUrls = image.match(/\b(https):\/\/(\S*)\b/);
 
-    var theImageLink = listOfUrls[0];
     // returns the album artwork image
+    theImageLink = listOfUrls[0];
 
-    var fixedURL = theImageLink.replace('&quot','');
+    fixedURL = theImageLink.replace('&quot','');
 
-    window.open(fixedURL);
     // opens the link in a new tab
+    window.open(fixedURL);
 }
 
+
 // var newButton = document.getElementById(getmyurlimageplease).addEventListener("click", getMyImagePlease);
-var newButton = document.getElementById("getMyUrlImagePlease").addEventListener("click", function() { getMyImagePlease(); });
+newButton = document.querySelector("#getMyUrlImagePlease").addEventListener("click", () =>  getMyImagePlease() );
+console.log(fixedURL)
